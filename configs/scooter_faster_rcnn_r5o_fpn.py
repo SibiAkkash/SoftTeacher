62c841b69606a374fc1f786256f50995799765f4
SibiAkkash <sibi.akkash@gmail.com>
2022-04-02 13:15:17 +0530

    modify faster-rcnn-scooter config

--- configs/scooter_faster_rcnn_r5o_fpn.py
+++ configs/scooter_faster_rcnn_r5o_fpn.py
@@ -8,22 +8,26 @@ model = dict(
     )
 )
 
+dataset_type = "COCODataset"
+classes = ("scooter",)
 data = dict(
     samples_per_gpu=4,
     workers_per_gpu=2,
     train=dict(
         ann_file="../yolov5/experiments/scooter-ds-cp/annotations/train.json",
         img_prefix="../yolov5/experiments/scooter-ds-cp/images/train/",
-    
+        classes=classes,
     ),
     val=dict(
         ann_file="../yolov5/experiments/scooter-ds-cp/annotations/val.json",
         img_prefix="../yolov5/experiments/scooter-ds-cp/images/val/",
+        classes=classes,
     ),
     test=dict(
         ann_file="../yolov5/experiments/scooter-ds-cp/annotations/val.json",
         img_prefix="../yolov5/experiments/scooter-ds-cp/images/val/",
-    )
+        classes=classes,
+    ),
 )
 
-load_from = "../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth"
+load_from = "/media/sibi/DATA/dev/ai/internship/SoftTeacher/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth"
